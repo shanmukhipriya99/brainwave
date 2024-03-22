665d45b3d9aa8d87a0618feedc4be3bd6bafd610
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2024-03-22 15:38:58 -0500

    hero section complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,14 +13,14 @@ const Hero = () => {
   const parallaxRef = useRef(null);
   return (
     <Section
-      className='pt-[12rem] -mt-[5.25]'
+      className='pt-[12rem] -mt-[5.25rem]'
       crosses
       crossesOffset='lg:translate-y-[5.25rem]'
       customPaddings
       id='hero'
     >
       <div className='container relative' ref={parallaxRef}>
-        <div className='relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]'>
+        <div className='relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]'>
           <h1 className='h1 mb-6'>
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className='inline-block relative'>

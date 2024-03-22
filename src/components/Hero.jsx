7f3c8678f4a2e0df32c2fa432c13d60563c6a609
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2024-03-22 15:29:06 -0500

    hero section-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,8 @@ import { heroIcons } from '../constants';
 import { ScrollParallax } from 'react-just-parallax';
 import { useRef } from 'react';
 import Generating from './Generating';
+import Notification from './Notification';
+import CompanyLogos from './CompanyLogos';
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -52,7 +54,9 @@ const Hero = () => {
                   height={490}
                   alt='AI'
                 />
-                <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2' />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2' />
+                </ScrollParallax>
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className='hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex'>
                     {heroIcons.map((icon, index) => (
@@ -61,6 +65,12 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className='hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex'
+                    title='Code Generation'
+                  />
                 </ScrollParallax>
               </div>
             </div>
@@ -77,6 +87,7 @@ const Hero = () => {
           </div>
           <BackgroundCircles />
         </div>
+        <CompanyLogos className='hidden relative z-10 mt-20 lg:block' />
       </div>
       <BottomLine />
     </Section>
